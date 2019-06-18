96f24cfb50e32b3e3b7787702aa65882c73b790d
root <root@DESKTOP-C0P11II.localdomain>
2019-06-18 06:16:45 +0200

    Update user nav

--- resources/views/navigation/user/top.blade.php
+++ resources/views/navigation/user/top.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-      <a class="navbar-brand" href="index.html">Logo</a>
+      <a class="navbar-brand" href="/">Logo</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
           <ul class="nav navbar-nav menu_nav justify-content-end">
               <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home</a>
+                  <a class="nav-link" href="/home">Home</a>
               </li>
               <li class="nav-item submenu dropdown">
                   <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Packages</a>
