604a3702c7af9bee58712bbe9c8dd9cc383c2acd
Azemira Mahmutbegovic <azemira.mah@gmail.com>
2019-06-19 00:09:21 +0200

    Error 404 template
    Error 404 template

--- frontend-templates/assets/js/main.js
+++ frontend-templates/assets/js/main.js
@@ -1,9 +1,9 @@
-$(function () {
-    if($('.btn-rotate-form').length){
-        $('.btn-rotate-form').on('click',function () {
+$(function() {
+    if ($('.btn-rotate-form').length) {
+        $('.btn-rotate-form').on('click', function() {
             $('.show').removeClass('show');
             var openFormID = $(this).data('openformid')
-            $('#'+openFormID).addClass('show')
+            $('#' + openFormID).addClass('show')
         });
     }
 })
