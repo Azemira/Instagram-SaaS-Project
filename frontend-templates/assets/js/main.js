faed7889b294d89005e8c10d66ac311ccfb3647a
Azemira Mahmutbegovic <azemira.mah@gmail.com>
2019-07-04 18:42:08 +0200

    Safari (on Mac) sections fixes, error page safari adjustments
    Safari (on Mac) sections fixes, error page safari adjustments

--- frontend-templates/assets/js/main.js
+++ frontend-templates/assets/js/main.js
@@ -1,9 +1,20 @@
-$(function() {
+$(function () {
     if ($('.btn-rotate-form').length) {
-        $('.btn-rotate-form').on('click', function() {
+        $('.btn-rotate-form').on('click', function () {
             $('.show').removeClass('show');
             var openFormID = $(this).data('openformid')
             $('#' + openFormID).addClass('show')
         });
+    }
+
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.indexOf('safari') != -1) {
+        if (ua.indexOf('chrome') > -1) {
+             // Chrome
+             $("body").addClass("chrome")
+        } else {
+            // Safari
+            $("body").addClass("safari")
+        }
     }
 })
