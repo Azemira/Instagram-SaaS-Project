26375c1c402900e6abc796e4fc34fa2a538750f7
Azemira Mahmutbegovic <azemira.mah@gmail.com>
2019-07-15 20:37:31 +0200

    admin dashboard basic layout, admin side menu, basic dashboard elements

---
+++ frontend-templates/assets/admin/js/admin-main.js
@@ -0,0 +1,22 @@
+$(function () {
+    $(".preloader").fadeOut();
+
+    $('#sidebarnav a').on('click', function (e) {
+        if (!$(this).hasClass("active")) {
+            // hide any open menus and remove all other classes
+            $("ul", $(this).parents("ul:first")).removeClass("in");
+            $("a", $(this).parents("ul:first")).removeClass("active");
+            // open our new menu and add the open class
+            $(this).next("ul").addClass("in");
+            $(this).addClass("active");
+        } else if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).parents("ul:first").removeClass("active");
+            $(this).next("ul").removeClass("in");
+        }
+    })
+    $('#sidebarnav >li >a.has-arrow').on('click', function (e) {
+        e.preventDefault();
+    });
+
+})
