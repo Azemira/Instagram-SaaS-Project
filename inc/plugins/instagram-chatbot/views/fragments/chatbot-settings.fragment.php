0f51d3626b3ba8956e6b2b50ab962bf826bc23d2
root <root@DESKTOP-8DHO4GJ.localdomain>
2019-08-23 17:33:27 +0200

    js fix

--- inc/plugins/instagram-chatbot/views/fragments/chatbot-settings.fragment.php
+++ inc/plugins/instagram-chatbot/views/fragments/chatbot-settings.fragment.php
@@ -1,69 +1,68 @@
 <div class="section-content">
-                    <div class="form-result mb-25"></div>
+    <div class="form-result mb-25"></div>
 
-                    <div class="clearfix">
+    <div class="clearfix">
 
-                            <div class="clearfix mb-40">
-                                <!-- <div class="col s6 m6 l6">
-                                    <label class="form-label">Speed</label>
+        <div class="clearfix mb-40">
+            <!-- <div class="col s6 m6 l6">
+<label class="form-label">Speed</label>
 
-                                    <select class="input" name="speed">
-                                        <option value="auto">Not Ready yet</option>
-                                     
-                                    </select>
-                                </div> -->
+<select class="input" name="speed">
+<option value="auto">Not Ready yet</option>
 
-                                <div class="col s6 s-last m6 m-last l6 l-last">
-                                    <label class="form-label">Status</label>
+</select>
+</div> -->
 
-                                    <select class="input" id="chatbot_status_select" name="is_active">
-                                    <?php if ($Settings->chatbot_status) { ?>
-                                        <option value="0" >Deactive</option>
-                                        <option value="1" selected="">Active</option>
-                                    <?php } else {?>
-                                        <option value="0"  selected="">Deactive</option>
-                                        <option value="1">Active</option>
-                                        <?php } ?>
-                                    </select>
-                                </div>
-                            </div>
+            <div class="col s6 s-last m6 m-last l6 l-last">
+                <label class="form-label">Status</label>
 
-                            <div class="clearfix">
-                                <div class="col s12 m6 l6">
-                                    <button class="fluid button chatbot_status" id="<?= APPURL?>/chatbot/settings/<?= $Account->get("id"); ?>" >Save</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="clearfix mb-40">
-                        <div class="stats-box">
-          <div class="stats-head">
-            <h3><span>Error</span> Log</h3>
-          
-          </div>
-          <table class="tb-stats">
-            <tbody>
-                <tr>
-                    <td><strong>Date</strong></td>
-                    <td><strong>Action</strong></td>
-                    <td><strong>Error</strong></td>
-                </tr>
-                <?php if($ErrorLog): ?>
-                    <?php foreach($ErrorLog as $error): ?>
+                <select class="input" id="chatbot_status_select" name="is_active">
+                    <?php if ($Settings->chatbot_status) { ?>
+                    <option value="0">Deactive</option>
+                    <option value="1" selected="">Active</option>
+                    <?php } else { ?>
+                    <option value="0" selected="">Deactive</option>
+                    <option value="1">Active</option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+
+        <div class="clearfix">
+            <div class="col s12 m6 l6">
+                <button class="fluid button chatbot_status" id="<?= APPURL ?>/chatbot/settings/<?= $Account->get("id"); ?>">Save</button>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix mb-40">
+        <div class="stats-box">
+            <div class="stats-head">
+                <h3><span>Error</span> Log</h3>
+
+            </div>
+            <table class="tb-stats">
+                <tbody>
                     <tr>
-                    <td data-label="Date"><?= $error->date ?></td>
-                    <td data-label="Action">
-                    <?= $error->error_action ?>
-                    </td>
-                    <td data-label="Error">
-                    <?= $error->error_message ?>
-                    </td>
+                        <td><strong>Date</strong></td>
+                        <td><strong>Action</strong></td>
+                        <td><strong>Error</strong></td>
+                    </tr>
+                    <?php if ($ErrorLog) : ?>
+                    <?php foreach ($ErrorLog as $error) : ?>
+                    <tr>
+                        <td data-label="Date"><?= $error->date ?></td>
+                        <td data-label="Action">
+                            <?= $error->error_action ?>
+                        </td>
+                        <td data-label="Error">
+                            <?= $error->error_message ?>
+                        </td>
                     </tr>
                     <?php endforeach ?>
-                <?php endif ?>
-            </tbody>
-      
-        </table>
+                    <?php endif ?>
+                </tbody>
+
+            </table>
         </div>
-                        </div>
-                    </div>
+    </div>
 </div>
